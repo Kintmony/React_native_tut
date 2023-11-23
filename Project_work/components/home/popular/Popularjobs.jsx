745a06c86385d30cd7b153958dcea46114c4c8cf
Tmony <66956192+Kintmony@users.noreply.github.com>
2023-11-23 13:57:22 +0000

    updated gitignore _layout and popularjobs component

--- Project_work/components/home/popular/Popularjobs.jsx
+++ Project_work/components/home/popular/Popularjobs.jsx
@@ -10,6 +10,7 @@ import { useRouter } from "expo-router";
 import styles from "./popularjobs.style";
 import { COLORS, SIZES } from "../../../constants";
 import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import useFetch from "../../../hook/useFetch";
 
 const router = useRouter();
 const isLoading = false;
@@ -33,6 +34,9 @@ const Popularjobs = () => {
           <FlatList
             data={[1, 2, 3, 4]}
             renderItem={({ item }) => <PopularJobCard item={item} />}
+            keyExtractor={(item) => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
           />
         )}
       </View>
